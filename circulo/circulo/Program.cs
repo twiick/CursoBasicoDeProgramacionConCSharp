253ac8135acf929b839b7aacb1f3640f3aef215f
twiickcom <twiick.com@gmail.com>
2023-06-16 08:04:29 -0500

    Se crea el código para calcular el área de un circulo

---
+++ circulo/circulo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+var radio = 0d;
+var resultado = 0d;
+
+Console.WriteLine("Bienvenido a la calculadora del área del circulo");
+Console.WriteLine("Ingresal el valor del radio");
+radio = Convert.ToDouble(Console.ReadLine());
+resultado = Math.PI * Math.Pow(radio, 2);
+Console.WriteLine("El área del circulo es: " + resultado);
