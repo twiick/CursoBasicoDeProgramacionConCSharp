07d5c516126dd3dd960c41c2073cd925bc130441
twiickcom <twiick.com@gmail.com>
2023-06-16 06:47:30 -0500

    Se añade el código para calcular el área de un rectángulo

--- helloWorld/helloWorld/Program.cs
+++ helloWorld/helloWorld/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola, esto es un mensaje de consola");
+// Console.WriteLine("Hola, esto es un mensaje de consola");
+// Programa que calcula el área de un rectángulo
+double ladoA;
+double ladoB;
+double area;
+Console.WriteLine("Calcula el área de un rectángulo");
+Console.WriteLine("Ingrese el valor del lado A");
+ladoA = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el valor del lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+area = ladoA * ladoB;
+Console.WriteLine("Lado A: " + ladoA);
+Console.WriteLine("Lado B: " + ladoB);
+Console.WriteLine("El resultado del área es: " + area);
