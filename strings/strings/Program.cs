cd9a45ccaaee0319a5c28dfa31b8717b58999acf
twiickcom <twiick.com@gmail.com>
2023-06-17 15:08:16 -0500

    Se crea el código para poner en práctica el manejo de los tipo de dato string

---
+++ strings/strings/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.WriteLine("Brahyam Luna ID!");
+int altura = 172;
+int edad = 21;
+string nombre = "Brahyam Luna";
+string informacion = "Nació en Colombia y es Diseñador Interactivo";
+string hobby = "Programar";
+string tarjetaDeIdentificacion = $"La información de {nombre} es la siguiente:\nSu edad es: {edad} años\nSu estatura es: {altura} cm\nInformación relevante: {informacion}\nSu hobby es: {hobby}";
+
+Console.WriteLine(tarjetaDeIdentificacion);
