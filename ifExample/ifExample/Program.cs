3d6d6b2391956fde97272e2a9478af85544a43c5
twiickcom <twiick.com@gmail.com>
2023-06-17 15:38:16 -0500

    Se crea el código para poner en práctica el uso del condicional if

---
+++ ifExample/ifExample/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int totalJugaror = 20;
+int totalDealer = 15;
+string mensaje = "";
+
+if (totalJugaror > totalDealer && totalJugaror < 22)
+{
+    mensaje = "Venciste al Dealer, felicidades";
+}
+else if (totalJugaror >= 22 || totalJugaror < totalDealer)
+{
+    mensaje = "Perdiste vs el Dealer, lo siento";
+}
+else
+{
+    mensaje = "Condición no valida";
+}
+
+Console.WriteLine(mensaje);
