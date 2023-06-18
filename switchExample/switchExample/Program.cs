fd31d4bf9bb97a00ee9ee6ace56042477fa1584b
twiickcom <twiick.com@gmail.com>
2023-06-18 15:19:27 -0500

    Se edita el código de switchExample para hacer el juego de 21, incluyendo al código el uso de ciclos while y do while

--- switchExample/switchExample/Program.cs
+++ switchExample/switchExample/Program.cs
@@ -1,36 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-int totalJugaror = 20;
-int totalDealer = 15;
+int totalJugaror = 0;
+int totalDealer = 0;
 string mensaje = "";
-
+int num = 0;
 string switchControl="menu";
+string controlOtraCarta = "";
 
-switch (switchControl)
+System.Random random = new System.Random();
+while (true)
 {
-    case "menu":
-        Console.WriteLine("Bienvenido al casino");
-        Console.WriteLine("Escriba 21 para jugar al 21");
-        switchControl = Console.ReadLine();
-        break;
-    case "21":
-        if (totalJugaror > totalDealer && totalJugaror < 22)
-        {
-            mensaje = "Venciste al Dealer, felicidades";
-        }
-        else if (totalJugaror >= 22 || totalJugaror < totalDealer)
-        {
-            mensaje = "Perdiste vs el Dealer, lo siento";
-        }
-        else
-        {
-            mensaje = "Condición no valida";
-        }
-        Console.WriteLine(mensaje);
-        break;
-    default:
-        Console.WriteLine("Valor ingresado no valido en el casino");
-        break;
-}
+    totalJugaror = 0;
+    totalDealer = 0;
+    switch (switchControl)
+    {
+        case "menu":
+            Console.WriteLine("Bienvenido al casino");
+            Console.WriteLine("Escriba 21 para jugar al 21");
+            switchControl = Console.ReadLine();
+            break;
+        case "21":
+            do
+            {
+                num = random.Next(1, 12);
+                totalJugaror = totalJugaror + num;
+                Console.WriteLine("Toma tu carta");
+                Console.WriteLine($"Te salio el: {num}");
+                Console.WriteLine("¿Deseas otra carta?");
+                controlOtraCarta = Console.ReadLine();
+            } while (controlOtraCarta == "Si" || controlOtraCarta == "si" || controlOtraCarta == "Yes" || controlOtraCarta == "yes");
 
+            totalDealer = random.Next(12, 23);
+            Console.WriteLine($"El dealer tiene: {totalDealer}");
+            
+            if (totalJugaror > totalDealer && totalJugaror <= 21 || totalDealer > 21)
+            {
+                mensaje = "Venciste al Dealer, felicidades";
+            }
+            else if (totalJugaror >= 22 || totalJugaror < totalDealer)
+            {
+                mensaje = "Perdiste vs el Dealer, lo siento";
+            }
+            else
+            {
+                mensaje = "Condición no valida";
+            }
+            Console.WriteLine(mensaje);
+            switchControl = "menu";
+            break;
+        default:
+            Console.WriteLine("Valor ingresado no valido en el casino");
+            break;
+    }
+}
